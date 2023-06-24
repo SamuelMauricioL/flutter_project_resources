0980f93862d4ae4a0ed0e75d725635a74bad39ae
Samuel Mauricio Laime <67238016+SamuelMauricioL@users.noreply.github.com>
2023-06-23 21:22:28 -0500

    Update transaction_middleware.dart

--- lib/pages/transaction/transaction_middleware.dart
+++ lib/pages/transaction/transaction_middleware.dart
@@ -4,14 +4,6 @@ import 'package:get/get.dart';
 class TransactionGuard extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    // final path = route?.split('/');
-    // print('TransactionGuard redirect: $path');
-    // if (path == null || path.length == 2) {
-    //   return null;
-    // }
-    // if (path.length > 2) {
-    //   return const RouteSettings(name: Routes.details);
-    // }
     return null;
   }
 }
