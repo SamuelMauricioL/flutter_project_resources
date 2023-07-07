a7facc06c98a0327ec1b3b579391f1c073cd0103
JulissaPonte <66703701+julissaponte@users.noreply.github.com>
2023-07-07 00:03:26 -0500

    MOBILE-2 (#7)
    * chore: add transaction functional test
    
    * chore: fix files related to transaction functional test

--- lib/core/testing/common/controller_injection.dart
+++ lib/core/testing/common/controller_injection.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_project_resources/pages/home/home_controller.dart';
 import 'package:flutter_project_resources/pages/product/product_controller.dart';
+import 'package:flutter_project_resources/pages/transaction/transaction_controller.dart';
 import 'package:get/get.dart';
 
 class ControllerInjection {
   static Future<void> init() async {
     Get.lazyPut(() => HomeController());
     Get.lazyPut(() => ProductController());
+    Get.lazyPut(() => TransactionController());
   }
 }
