5d6087f82ca87613140e7a65a2f02e888265a06c
Samuel-ML <samuel.mauricio@ext.blaze.me>
2023-07-06 19:50:42 -0500

    refactor: remove unused code

--- lib/pages/details/details_middleware.dart
+++ lib/pages/details/details_middleware.dart
@@ -4,7 +4,6 @@ import 'package:get/get.dart';
 class DetailsGuard extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    print('DetailsGuard redirect: $route');
     return null;
   }
 }
--- lib/pages/product/product_middleware.dart
+++ lib/pages/product/product_middleware.dart
@@ -4,14 +4,6 @@ import 'package:get/get.dart';
 class ProductGuard extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
-    // final path = route?.split('/');
-    // print('ProductGuard redirect: $path');
-    // if (path == null || path.length == 2) {
-    //   return null;
-    // }
-    // if (path.length > 2) {
-    //   return const RouteSettings(name: Routes.details);
-    // }
     return null;
   }
 }
