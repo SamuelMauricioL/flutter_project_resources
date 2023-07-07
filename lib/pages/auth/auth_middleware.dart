ab090b4b6df317c611ed1834021d9e66f72f98e2
Samuel-ML <samuel.mauricio@ext.blaze.me>
2023-07-06 20:30:59 -0500

    refactor: remove unused code

--- lib/pages/auth/auth_middleware.dart
+++ lib/pages/auth/auth_middleware.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project_resources/routes/app_pages.dart';
+// import 'package:flutter_project_resources/routes/app_pages.dart';
 import 'package:get/get.dart';
 
 class AuthGuard extends GetMiddleware {
@@ -8,8 +8,9 @@ class AuthGuard extends GetMiddleware {
     const isAuth = true;
     if (isAuth) {
       return null;
-    } else {
-      return const RouteSettings(name: Routes.login);
     }
+    // else {
+    //   return const RouteSettings(name: Routes.login);
+    // }
   }
 }
